bee4d23d719feeea20c3e8e52e56151707dda0e4
Gilmar Alexandre do Prado Yahuita <gilmaralexandreprado@gmail.com>
2023-08-23 19:12:08 -0300

    Terceiro Commit - Gilmar - 23-08-2023

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # api-brazcubas
+## Instalação
+
+Certifique-se de ter o Node.js instalado. Clone este repositório e execute:
+
+```bash
+npm install
--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  res.send("Hello")
 });
 
 app.listen(port, () => {
-  console.log(`Iniciando o serviodr na porta:${port}`);
+  console.log(`Iniciando o servidor na porta:${port}`);
 });
